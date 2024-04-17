9c452383df350aad5a13c23228df4844cf5e5f87
Gurkan <hello@gurkz.me>
2024-04-17 17:27:05 +0200

    add link statistics

--- src/app/dashboard/links/[id]/page.tsx
+++ src/app/dashboard/links/[id]/page.tsx
@@ -3,6 +3,45 @@ import { deleteLinkAction } from "@/lib/link/actions";
 import { getLinkById, getLinkClicks } from "@/lib/link/links";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+import { InfoIcon } from "lucide-react";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+
+async function LinkStatistics({ linkId }: { linkId: string }) {
+  const linkClicks = await getLinkClicks(linkId);
+
+  if (!linkClicks || linkClicks.length === 0)
+    return (
+      <div className="py-4">
+        <Alert>
+          <InfoIcon className="h-4 w-4" />
+          <AlertTitle>No statistics yet.</AlertTitle>
+          <AlertDescription>
+            No one has used your link yet. When they do, you will be able to see
+            it here.
+          </AlertDescription>
+        </Alert>
+      </div>
+    );
+
+  return (
+    <div className="py-4">
+      <h3 className="text-xl font-bold">link statistics</h3>
+      <div className="grid grid-cols-2 md:grid-cols-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+            <CardTitle className="text-lg font-medium">
+              Link Clicks (lifetime)
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <div className="text-2xl font-bold">{linkClicks?.length}</div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
 
 export default async function LinkDetails({
   params,
@@ -13,15 +52,13 @@ export default async function LinkDetails({
 
   if (!link) redirect("/dashboard");
 
-  const linkClicks = await getLinkClicks(link.id);
-
   return (
     <>
       <Button className="w-fit">
         <Link href={"/dashboard"}>back to dashboard</Link>
       </Button>
       <h2 className="text-4xl">link: {link.slug}</h2>
-      <p>this page is under construction, clicks: {linkClicks?.length}</p>
+      <LinkStatistics linkId={link.id} />
 
       <form action={deleteLinkAction}>
         <input type="hidden" name="id" value={link.id} />
