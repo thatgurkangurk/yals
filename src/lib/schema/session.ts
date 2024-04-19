f98abfd22a30caec2e8730b9defb45f965403a9e
Gurkan <hello@gurkz.me>
2024-04-19 19:47:19 +0200

    install lucia auth and remove default session id (lucia generates that)

--- src/lib/schema/session.ts
+++ src/lib/schema/session.ts
@@ -1,12 +1,8 @@
 import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
-import { nanoid } from "nanoid";
 import { users } from "./user";
 
 export const sessions = sqliteTable("session", {
-  id: text("id")
-    .primaryKey()
-    .$defaultFn(() => nanoid(21))
-    .unique(),
+  id: text("id").primaryKey().unique(),
   userId: text("user_id")
     .notNull()
     .references(() => users.id),
