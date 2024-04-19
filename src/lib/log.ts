9e7d7461af7ac99a0b993898250e39b396b2d8eb
Gurkan <hello@gurkz.me>
2024-04-19 09:11:09 +0200

    add custom logger

---
+++ src/lib/log.ts
@@ -0,0 +1,34 @@
+import { consola } from "consola";
+
+function info(message: string) {
+    consola.info(message);
+}
+
+function log(message: string) {
+    consola.log(message);
+}
+
+function warn(message: string) {
+    consola.warn(message);
+}
+
+function error(message: string) {
+    consola.error(message);
+}
+
+function start(message: string) {
+    consola.start(message);
+}
+
+function box(message: string) {
+    consola.box(message);
+}
+
+export {
+    info,
+    log,
+    warn,
+    start,
+    error,
+    box
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
+import { box } from "@/lib/log";
 
 const app = new Hono();
 app.get("/", (c) => c.text("yals"));
 app.get("/healthcheck", (c) => c.text("OK"));
+
+box("yals hono rewrite");
 
 export default {
     port: 3000,
