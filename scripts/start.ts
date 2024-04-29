4418b15893ade72c88fff98c2ddfa6aa70bab550
Gurkan <hello@gurkz.me>
2024-04-29 10:47:12 +0200

    setup data directory

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,8 +1,21 @@
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { db } from "../src/lib/db";
 import { startServer } from "./lib/server";
+import { env } from "../src/env";
+import { existsSync } from "node:fs";
+import { mkdir } from "node:fs/promises";
+
+function setupDataDir() {
+   if (existsSync(env.DATA_DIR) {
+     console.log(`data dir is ${env.DATA_DIR}`);
+     return;
+   }
+   console.log("creating data directory at " + env.DATA_DIR);
+   await mkdir(env.DATA_DIR, { recursive: true });
+}
 
 try {
+  setupDataDir();
   migrate(db, { migrationsFolder: "./drizzle" });
 } catch (err) {
   console.error(err);
