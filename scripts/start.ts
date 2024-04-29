6129de5fba0254b0142d28b377ba0226c37ebdb4
Gurkan <hello@gurkz.me>
2024-04-29 11:26:15 +0200

    fix db issue?

--- scripts/start.ts
+++ scripts/start.ts
@@ -1,9 +1,19 @@
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
-import { db } from "../src/lib/db";
+import { drizzle } from "drizzle-orm/bun-sqlite";
 import { startServer } from "./lib/server";
 import { env } from "../src/env";
 import { existsSync } from "node:fs";
 import { mkdir } from "node:fs/promises";
+import { Database } from "bun:sqlite";
+import * as userSchema from "../src/lib/db/schema/user";
+import * as sessionSchema from "../src/lib/db/schema/session";
+import * as linkSchema from "../src/lib/db/schema/link";
+
+const schema = {
+  ...userSchema,
+  ...sessionSchema,
+  ...linkSchema,
+};
 
 async function setupDataDir() {
    if (existsSync(env.DATA_DIR)) {
@@ -14,9 +24,15 @@ async function setupDataDir() {
    await mkdir(env.DATA_DIR, { recursive: true });
 }
 
+async function migrateDB() {
+  const sqlite = new Database(`${env.DATA_DIR}/yals.db`, { create: true });
+  const db = drizzle(sqlite, { schema: schema });
+  migrate(db, { migrationsFolder: "./drizzle" });
+}
+
 try {
   await setupDataDir();
-  migrate(db, { migrationsFolder: "./drizzle" });
+  await migrateDB();
 } catch (err) {
   console.error(err);
   process.exit(1);
