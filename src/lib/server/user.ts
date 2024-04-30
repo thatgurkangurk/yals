5e576ded2c0cd63180ff2a2ed98190ea7c80e207
Gurkan <hello@gurkz.me>
2024-04-30 12:39:01 +0200

    wrap user creation

--- src/lib/server/user.ts
+++ src/lib/server/user.ts
@@ -1,4 +1,7 @@
 import { db } from "$lib/db";
+import { users } from "$lib/db/schema/user";
+import { generateId, type User } from "lucia";
+import { hashPassword } from "./password";
 
 async function userExists(username: string) {
     const existingUser = await db.query.users.findFirst({
@@ -8,6 +11,53 @@ async function userExists(username: string) {
     return existingUser;
 }
 
+async function createUser({
+    username,
+    password
+}: {
+    username: string,
+    password: string
+}): Promise<{
+    success: true,
+    user: User
+} | {
+    success: false,
+    cause: "USER_EXISTS" | "UNEXPECTED_ERROR"
+}> {
+    if (await userExists(username)) return {
+        success: false,
+        cause: "USER_EXISTS"
+    }
+
+    const id = generateId(15);
+    const hashedPassword = await hashPassword(password);
+
+    try {
+        const insertedUser = await db.insert(users).values({
+            id: id,
+            username: username,
+            hashed_password: hashedPassword
+        }).returning({
+            id: users.id,
+            username: users.username,
+            role: users.role
+        })
+
+        return {
+            success: true,
+            user: insertedUser[0]
+        }
+    } catch (e) {
+        return {
+            success: false,
+            cause: "UNEXPECTED_ERROR"
+        }
+    }
+
+
+}
+
 export {
-    userExists
+    userExists,
+    createUser
 }
--- src/routes/auth/register/+page.server.ts
+++ src/routes/auth/register/+page.server.ts
@@ -1,15 +1,11 @@
 import { lucia } from "$lib/server/auth";
 import { fail, redirect } from "@sveltejs/kit";
-import { generateId } from "lucia";
 import type { Actions, PageServerLoad } from "./$types";
-import { db } from "$lib/db";
-import { users } from "$lib/db/schema/user";
 import { registerFormSchema } from "$lib/user";
 import { setError, superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { getServerSettingsOrInit } from "$lib/serverSettings";
-import { hashPassword } from "$lib/server/password";
-import { userExists } from "$lib/server/user";
+import { createUser } from "$lib/server/user";
 
 export const load: PageServerLoad = async (event) => {
   if (event.locals.user) {
@@ -40,24 +36,27 @@ export const actions: Actions = {
 
     const { username, password } = form.data;
 
-    const userId = generateId(15);
-    const hashedPassword = await hashPassword(password);
+    const result = await createUser({
+      username,
+      password
+    });
 
-    if (await userExists(username)) {
-      setError(form, "username", "username is not unique");
-      return fail(400, { form: form });
+    if (!result.success) {
+      switch (result.cause) {
+        case "USER_EXISTS": {
+          setError(form, "username", "a user with that username already exists");
+          return fail(409, { form: form });
+        }
+        case "UNEXPECTED_ERROR": {
+          setError(form, "something unexpected went wrong, please try again later");
+          return fail(500, { form: form });
+        }
+      }
     }
 
-    try {
-      await db.insert(users).values({
-        id: userId,
-        username: username,
-        hashed_password: hashedPassword,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-    const session = await lucia.createSession(userId, {});
+    const { user } = result;
+    
+    const session = await lucia.createSession(user.id, {});
     const sessionCookie = lucia.createSessionCookie(session.id);
     event.cookies.set(sessionCookie.name, sessionCookie.value, {
       path: ".",
