45e42bccdbc16a78f13f2ea2ec590d95f79bf9b0
Gurkan <hello@gurkz.me>
2024-04-30 16:17:09 +0200

    fix footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,3 +1,8 @@
-<div class="sticky bottom-0 px-2 py-4 border-t-2">
-    <p>this server is using <a href="https://github.com/thatgurkangurk/yals" class="underline text-primary underline-offset-4">yals</a></p>
+<div class="px-2 py-4 border-t-2 bg-background">
+  <p>
+    this server is using <a
+      href="https://github.com/thatgurkangurk/yals"
+      class="underline text-primary underline-offset-4">yals</a
+    >
+  </p>
 </div>
