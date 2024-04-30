440b9f4df3a488e8b9ecad619df00f574dccd9cd
Gurkan <hello@gurkz.me>
2024-04-30 16:09:52 +0200

    set first user to admin

--- src/lib/server/user.ts
+++ src/lib/server/user.ts
@@ -2,62 +2,80 @@ import { db } from "$lib/db";
 import { users } from "$lib/db/schema/user";
 import { generateId, type User } from "lucia";
 import { hashPassword } from "./password";
+import { sql } from "drizzle-orm";
 
 async function userExists(username: string) {
-    const existingUser = await db.query.users.findFirst({
-        where: (user, { eq }) => eq(user.username, username)
-    });
+  const existingUser = await db.query.users.findFirst({
+    where: (user, { eq }) => eq(user.username, username),
+  });
 
-    return existingUser;
+  return existingUser;
+}
+
+async function getTotalUserCount() {
+  const [count] = await db.select({ count: sql<number>`count(*)` }).from(users);
+
+  return count.count;
+}
+
+async function isUsersEmpty() {
+  const count = await getTotalUserCount();
+  const isEmpty = count === 0;
+
+  return isEmpty;
 }
 
 async function createUser({
-    username,
-    password
+  username,
+  password,
 }: {
-    username: string,
-    password: string
-}): Promise<{
-    success: true,
-    user: User
-} | {
-    success: false,
-    cause: "USER_EXISTS" | "UNEXPECTED_ERROR"
-}> {
-    if (await userExists(username)) return {
-        success: false,
-        cause: "USER_EXISTS"
+  username: string;
+  password: string;
+}): Promise<
+  | {
+      success: true;
+      user: User;
     }
-
-    const id = generateId(15);
-    const hashedPassword = await hashPassword(password);
-
-    try {
-        const insertedUser = await db.insert(users).values({
-            id: id,
-            username: username,
-            hashed_password: hashedPassword
-        }).returning({
-            id: users.id,
-            username: users.username,
-            role: users.role
-        })
-
-        return {
-            success: true,
-            user: insertedUser[0]
-        }
-    } catch (e) {
-        return {
-            success: false,
-            cause: "UNEXPECTED_ERROR"
-        }
+  | {
+      success: false;
+      cause: "USER_EXISTS" | "UNEXPECTED_ERROR";
     }
+> {
+  if (await userExists(username))
+    return {
+      success: false,
+      cause: "USER_EXISTS",
+    };
 
+  const id = generateId(15);
+  const hashedPassword = await hashPassword(password);
+  const isFirstUser = await isUsersEmpty();
 
+  try {
+    const insertedUser = await db
+      .insert(users)
+      .values({
+        id: id,
+        username: username,
+        hashed_password: hashedPassword,
+        role: isFirstUser ? "admin" : "user",
+      })
+      .returning({
+        id: users.id,
+        username: users.username,
+        role: users.role,
+      });
+
+    return {
+      success: true,
+      user: insertedUser[0],
+    };
+  } catch (e) {
+    return {
+      success: false,
+      cause: "UNEXPECTED_ERROR",
+    };
+  }
 }
 
-export {
-    userExists,
-    createUser
-}
+export { userExists, createUser };
