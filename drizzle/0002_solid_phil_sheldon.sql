93dddce7ec1521a4b1426f33b549fb3ede1528f2
Gurkan <hello@gurkz.me>
2024-04-30 09:17:44 +0200

    change create table to create table if not exists (to prevent errors)

--- drizzle/0002_solid_phil_sheldon.sql
+++ drizzle/0002_solid_phil_sheldon.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `server_settings` (
+CREATE TABLE IF NOT EXISTS `server_settings` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`registration_enabled` integer DEFAULT true NOT NULL,
 	`footer_enabled` integer DEFAULT true NOT NULL
