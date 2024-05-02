5a811817164e8cf78c5a30544f7849f680eac3a0
Gurkan <hello@gurkz.me>
2024-05-02 10:31:06 +0200

    add basic user settings page

---
+++ src/routes/user/settings/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import type { PageData } from "./$types";
+    export let data: PageData;
+</script>
+
+<h2 class="text-3xl">user settings</h2>
+<p>currently logged in as: {data.user.username}</p>
+<p>wip</p>
